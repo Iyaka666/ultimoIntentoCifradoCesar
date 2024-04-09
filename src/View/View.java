27765032c5f247518f5338374b1788af0afa04d2
Iyaka666 <duendecum@gmail.com>
2024-04-08 22:14:18 -0500

    Creación
    Agregue lógica a la clase

--- src/View/View.java
+++ src/View/View.java
@@ -1,5 +1,30 @@
 package View;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class View {
   
+  private char[] key;
+
+  public void setVisible(boolean b){
+    String input = "";
+    
+    BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+    
+    System.out.println("Cifrador Caésar");
+    System.out.println("Ingrese la clave por ejemplo az");
+    try{
+      input = reader.readLine();
+      reader.close();
+    }catch(IOException ioE){
+      System.out.println(ioE.getMessage());
+    }
+    input.toLowerCase();
+    this.key = input.toCharArray();
+  }
+
+  public char[] getKey() {
+    return key;
+  }
 }
