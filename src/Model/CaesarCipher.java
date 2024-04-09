b80155db4d4bafc3e0e2464b67d11e29e08e7eda
Iyaka666 <duendecum@gmail.com>
2024-04-09 17:49:25 -0500

    Modificación

--- src/Model/CaesarCipher.java
+++ src/Model/CaesarCipher.java
@@ -1,40 +1,11 @@
 package Model;
 
-public class Caesar implements Desencrypter{
+public class CaesarCipher{
   private String text;
   private char[] key;
 
-  public Caesar(String text, char[] key) throws Exception{
-    this.text = text;
-    setKey(key);
+  public CaesarCipher(String text, char[] key) throws Exception{
+    this.text = text;    
   }
 
-  public char[] getKey() {
-    return key;
-  }
-
-  public void setKey(char[] key) throws Exception{
-    if(key.length > 2){
-      throw new Exception("it key only can expect two characters");
-    }
-    this.key = key;
-  }
-
-  public String getText() {
-    return text;
-  }
-
-  public void setText(String text) {
-    this.text = text;
-  }
-
-  public String cipher(){
-    return "";
-  }
-
-  @Override
-  public String descipher(String textEncrypted){
-    String desencrypted = "";
-    return desencrypted;
-  }
 }
