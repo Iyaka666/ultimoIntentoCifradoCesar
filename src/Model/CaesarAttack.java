5eabc62e63684d2ed6fabbd761b54be19838c136
Iyaka666 <duendecum@gmail.com>
2024-04-10 07:52:33 -0500

    Modificación de clases

--- src/Model/CaesarAttack.java
+++ src/Model/CaesarAttack.java
@@ -1,31 +1,73 @@
 package Model;
 
-public class CaesarAttack{
-  private String text;
+import java.util.Map;
+import java.util.HashMap;
 
-  public CaesarAttack(String text){
-    setText(text);
+public class CaesarAttack {
+  private String textEncrypt;
+  private String lettersMoreUseInEnglish;
+  private String alphabeth;
+
+  public CaesarAttack(String textEncrypt, String alphabeth, String letterMoreUse){
+    this.textEncrypt = textEncrypt;
+    this.alphabeth = alphabeth;
+    this.lettersMoreUseInEnglish = "ETAOINSHRDLCUMWFGYPBVKJXQZ";
   }
 
-  public String getText() {
-    return text;
+  public String getTextEncrypt() {
+    return textEncrypt;
   }
-
-  public void setText(String text) {
-    this.text = text.toLowerCase();
+  
+  public void setTextEncrypt(String textEncrypt) {
+    this.textEncrypt = textEncrypt;
   }
-
-  private int getRandom(int limitTop, int limitBottom){
-    return (int)Math.round( Math.random() * (limitBottom - limitTop) + limitTop );
+  
+  // Método para calcular la frecuencia de las letras en el texto
+  private Map<Character, Integer> calculatefrequecyLetters() {
+    
+    Map<Character, Integer> frequecyLetters = new HashMap<>();
+    
+    for (char character : this.textEncrypt.toCharArray()) {
+      if (Character.isLetter(character)) {
+        character = Character.toUpperCase(character);
+        frequecyLetters.put(character, frequecyLetters.getOrDefault(character, 0) + 1);
+      }
+    }
+    
+    return frequecyLetters;
   }
-
-  private char[] createRandomKey(){
-    char[] randomKey = {(char)getRandom(122, 97), (char)getRandom(122,97)};
-    return randomKey; 
+  
+  // Método para encontrar la letra más común en el texto
+  private char findLetterMoreCommon(Map<Character, Integer> frequecyLetters) {
+    
+    int maxFrequency = 0;
+    
+    char letterMoreCommon = ' ';
+    
+    for (Map.Entry<Character, Integer> entry : frequecyLetters.entrySet()) {
+      if (entry.getValue() > maxFrequency) {
+        maxFrequency = entry.getValue();
+        letterMoreCommon = entry.getKey();
+      }
+    }
+    
+    return letterMoreCommon;
   }
-
-  public String[] getTextsProbable(int limit){
-    String[] textsProbable = new String[limit];
-    return textsProbable;
+  
+  // Método para calcular el desplazamiento
+  private int calculateShift(char letterEncrypt, char letterOrigin) {
+    return (letterEncrypt - letterOrigin + alphabeth.length()) % alphabeth.length();
+  }
+  
+  public String getTextsProbable(){
+    // Calcular la frecuencia de las letras en el texto cifrado
+    Map<Character, Integer> frecuenciaLetras = calculatefrequecyLetters();
+    // Letra más común en el texto cifrado
+    char letraMasComun = findLetterMoreCommon(frecuenciaLetras);
+    // Calcular el desplazamiento
+    int desplazamiento = calculateShift(letraMasComun, 'E');
+    // Descifrar el texto cifrado con el desplazamiento calculado
+    String textoDescifrado = CaesarCipher.desencrypt(this.textEncrypt, desplazamiento);
+    return textoDescifrado; 
   }
 }
--- src/Model/CaesarCipher.java
+++ src/Model/CaesarCipher.java
@@ -1,11 +1,58 @@
 package Model;
+public class CaesarCipher {
 
-public class CaesarCipher{
-  private String text;
-  private char[] key;
+  private CaesarCipher(){}
 
-  public CaesarCipher(String text, char[] key) throws Exception{
-    this.text = text;    
-  }
+  public static String encrypt(String text, int key) {
 
+    StringBuilder textEncrypt = new StringBuilder();  
+  
+    for (char character : text.toCharArray()) {
+    
+      if (Character.isLetter(character)) {
+        boolean upper = Character.isUpperCase(character);
+        character = Character.toLowerCase(character);
+        int code = character - 'a';
+        int codeEncrypt = (code + key) % 26;
+        char characterEncrypt = (char) (codeEncrypt + 'a');
+    
+      if (upper) {
+        characterEncrypt = Character.toUpperCase(characterEncrypt);
+      }
+      textEncrypt.append(characterEncrypt);
+      } else {
+        textEncrypt.append(character);
+      }
+}
+return textEncrypt.toString();
+}
+
+  public static String desencrypt(String textEncrypt, int key) {
+    
+    StringBuilder textDesencrypt = new StringBuilder();
+    
+    for (char character : textEncrypt.toCharArray()) {
+      
+      if (Character.isLetter(character)) {
+        boolean upper = Character.isUpperCase(character);
+        character = Character.toLowerCase(character);
+        int code = character - 'a';
+        int codeDesencrypt = (code - key) % 26;
+      
+        if (codeDesencrypt < 0) {
+        codeDesencrypt += 26; // Manejo de números negativos
+        }
+      
+      char characterDesencrypt = (char) (codeDesencrypt + 'a');
+      
+      if (upper) {
+          characterDesencrypt = Character.toUpperCase(characterDesencrypt);
+      }
+      textDesencrypt.append(characterDesencrypt);
+      } else {
+        textDesencrypt.append(character);
+      }
+    }
+    return textDesencrypt.toString();
+}
 }
